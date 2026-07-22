1278aeef280fc4562cc8bc3ecd4f1cd4604df693
panchalvinay33-debug <panchalvinay33@gmail.com>
2026-07-22 15:06:25 +0530

    Mark users offline before sign out

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
+import 'presence_coordinator.dart';
+
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final GoogleSignIn _googleSignIn = GoogleSignIn();
@@ -11,8 +13,7 @@ class AuthService {
     try {
       await _googleSignIn.signOut();
 
-      final GoogleSignInAccount? googleUser =
-          await _googleSignIn.signIn();
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
 
       if (googleUser == null) return null;
 
@@ -24,13 +25,16 @@ class AuthService {
         idToken: googleAuth.idToken,
       );
 
-      return await _auth.signInWithCredential(credential);
-    } catch (e) {
+      final result = await _auth.signInWithCredential(credential);
+      await PresenceCoordinator.instance.markCurrentUserOnline();
+      return result;
+    } catch (_) {
       rethrow;
     }
   }
 
   Future<void> signOut() async {
+    await PresenceCoordinator.instance.markCurrentUserOffline();
     await _googleSignIn.signOut();
     await _auth.signOut();
   }
@@ -48,14 +52,14 @@ class AuthService {
     }
 
     try {
+      await PresenceCoordinator.instance.markCurrentUserOffline();
       await user.delete();
     } on FirebaseAuthException catch (e) {
       if (e.code != 'requires-recent-login') {
         rethrow;
       }
 
-      final GoogleSignInAccount? googleUser =
-          await _googleSignIn.signIn();
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
 
       if (googleUser == null) rethrow;
 
@@ -68,14 +72,17 @@ class AuthService {
       );
 
       await user.reauthenticateWithCredential(credential);
+      await PresenceCoordinator.instance.markCurrentUserOffline();
       await user.delete();
     }
   }
-Future<void> deleteAccount() async {
-  await deleteFirebaseAuthAccount();
-}
+
+  Future<void> deleteAccount() async {
+    await deleteFirebaseAuthAccount();
+  }
 
   Future<void> logout() async {
+    await PresenceCoordinator.instance.markCurrentUserOffline();
     await _googleSignIn.signOut();
     await _auth.signOut();
   }
