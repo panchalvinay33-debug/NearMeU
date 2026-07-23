dcfe13b44e1f802bb21974906d144aedd2c886e5
panchalvinay33-debug <panchalvinay33@gmail.com>
2026-07-23 08:28:15 +0530

    Attach safe notification navigator

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 import 'screens/auth_gate_screen.dart';
 import 'security/suspension_guard.dart';
+import 'services/notification_navigation_service.dart';
 import 'services/notification_service.dart';
 import 'widgets/presence_lifecycle.dart';
+
+final GlobalKey<NavigatorState> rootNavigatorKey = GlobalKey<NavigatorState>();
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+  NotificationNavigationService.instance.attachNavigatorKey(rootNavigatorKey);
   await NotificationService.instance.initialize();
 
   runApp(const NearMeUApp());
@@ -21,6 +27,7 @@ class NearMeUApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: rootNavigatorKey,
       title: 'NearMeU',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
