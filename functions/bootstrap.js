ff1aac99f7afa481f6f9ffd56f42897f5ca6a81e
panchalvinay33-debug <panchalvinay33@gmail.com>
2026-08-01 17:24:40 +0530

    Batch 05: export account lifecycle functions

--- functions/bootstrap.js
+++ functions/bootstrap.js
@@ -18,6 +18,7 @@ module.exports = {
   ...require("./private_media_ack_functions.js"),
   ...require("./message_unsend_functions.js"),
   ...require("./chat_clear_functions.js"),
+  ...require("./account_lifecycle_functions.js"),
   ...require("./announcement_media_functions.js"),
   ...require("./announcement_push_functions.js"),
   ...require("./app_version_functions.js"),
