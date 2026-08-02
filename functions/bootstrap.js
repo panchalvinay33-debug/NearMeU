f5cefec328d26fb7b8cf8ef1af05048bc50bcc3d
panchalvinay33-debug <panchalvinay33@gmail.com>
2026-08-02 21:55:55 +0530

    Batch 09: export audio call functions

--- functions/bootstrap.js
+++ functions/bootstrap.js
@@ -24,6 +24,7 @@ module.exports = {
   ...require("./chat_clear_functions.js"),
   ...require("./account_lifecycle_functions.js"),
   ...require("./profile_sharing_functions.js"),
+  ...require("./audio_call_functions.js"),
   ...require("./announcement_media_functions.js"),
   ...require("./announcement_push_functions.js"),
   ...require("./app_version_functions.js"),
